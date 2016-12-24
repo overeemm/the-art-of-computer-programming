6bb708edd6ca23d07bf36e14a46af6180d3ae4df
Michiel Overeem <overeemm@gmail.com>
2016-12-24 19:16:59 +0100

    First algorithm: 1.1E

---
+++ Algorithm1.1E/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let rec algorithmE m n =
+  if n = 0 then m
+  else algorithmE n (m % n)
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
