2abcbb69938741c17791607fddf5ffe98f7ccc9e
Michiel Overeem <overeemm@gmail.com>
2017-01-05 19:41:11 +0100

    Added algorithm M

---
+++ Algorithm1.2.10M/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Algorithm1._2._10M.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Algorithm1.2.10M")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Algorithm1.2.10M")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("533c9582-7da2-4761-938f-47913fd0d544")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Algorithm1.2.10M/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let algorithmM x =
+  let lastIndex = (Array.length x) - 1
+  let rec findMax j k m =
+    if k = 0 
+      then (j, m)
+      elif x.[k] <= m 
+        then findMax j (k - 1) m
+        else findMax k k x.[k]
+  findMax lastIndex (lastIndex - 1) x.[lastIndex]
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" (algorithmM [| 1; 2; 8; 4; 5; 6 |])
+    0 // return an integer exit code
